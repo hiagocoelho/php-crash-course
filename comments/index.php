9aaa86e6b7e5bce3cf89bdb60edb46e4f06ff08c
Hiago Coelho <b8coelho@gmail.com>
2023-04-06 12:19:56 -0300

    comments

---
+++ comments/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments</title>
+</head>
+<body>
+    <?php
+        // Single line comment
+        # Another form of making a single line comment
+        echo "<h1>Comments!<h1>";
+        /*
+            Multiline comments
+            with some
+            really nice
+            information!
+            :)
+        */
+    ?>
+</body>
+</html>
